177117e30ba401e522ab822c978ad34fdc90d298
eliot <eliotgiraud@gmail.com>
2025-10-28 14:25:14 +0100

    fix vitest exec e2e test and then failed

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     globals: true,
+    include: ['app/**/*.{test,spec}.{js,ts}'],
+    exclude: ['node_modules', 'e2e'],
   },
 })
 
