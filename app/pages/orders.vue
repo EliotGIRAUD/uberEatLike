81c8a483a09bcb4358e5997c28bc2b4d1eb1ec58
eliot <eliotgiraud@gmail.com>
2025-10-29 16:21:30 +0100

    fix

--- app/pages/orders.vue
+++ app/pages/orders.vue
@@ -64,7 +64,7 @@ const userStore = useUserStore()
 const orderStore = useOrderStore()
 
 useHead({
-  title: 'Mes Commando's - Grosmino\'s',
+  title: 'Mes Commandes - Grosmino\'s',
   meta: [
     { name: 'description', content: 'Suivez l\'état de vos commandes en temps réel.' },
     { name: 'robots', content: 'noindex, nofollow' },
